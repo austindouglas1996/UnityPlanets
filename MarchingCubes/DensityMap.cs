57d0c1c204904e65e69b7bc4b422fc387a0bf34e
Austin Douglas <austindouglas@outlook.com>
2025-05-19 19:27:19 -0400

    Removed densityMapA

--- MarchingCubes/DensityMap.cs
+++ MarchingCubes/DensityMap.cs
@@ -3,54 +3,6 @@ using System;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 
-public class DensityMapA
-{
-    private readonly float[] _values;
-    public readonly int SizeX, SizeY, SizeZ;
-
-    public DensityMapA(int x, int y, int z)
-    {
-        SizeX = x;
-        SizeY = y;
-        SizeZ = z;
-        _values = new float[x * y * z];
-    }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public int GetIndex(int x, int y, int z)
-    {
-        return x + SizeX * (y + SizeY * z);
-    }
-
-    public float Get(int x, int y, int z)
-    {
-        return _values[GetIndex(x, y, z)];
-    }
-
-    public void Set(int x, int y, int z, float value)
-    {
-        try
-        {
-            _values[GetIndex(x, y, z)] = value;
-        }
-        catch (System.Exception e)
-        {
-            Debug.LogException(e);
-        }
-    }
-
-    public float this[int x, int y, int z]
-    {
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        get => _values[GetIndex(x, y, z)];
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        set => _values[GetIndex(x, y, z)] = value;
-    }
-
-    public float[] Raw => _values;
-}
-
 public class DensityMap
 {
     private readonly float[] _values;
