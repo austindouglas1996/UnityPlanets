55d454579c29956c52e5101e8ef4e4131273a95a
Austin Douglas <austindouglas@outlook.com>
2025-08-13 16:47:50 -0400

    How the mighty have fallen. Deleted ChunkContext. Its use from over 50 references to 1 after implementing ChunkKey. This will speed things a little

--- Terrain/Engine/Utilities/ChunkGPURenderData.cs
+++ Terrain/Engine/Utilities/ChunkGPURenderData.cs
@@ -6,12 +6,14 @@ public class ChunkRegionRenderDataGPU
     public ComputeBuffer Triangle;
     public ComputeBuffer Args;
     public Bounds Bounds;
+    private IChunkLayout Layout;
 
-    public ChunkRegionRenderDataGPU(ComputeBuffer Triangle, ComputeBuffer Args, List<ChunkContext> contexts)
+    public ChunkRegionRenderDataGPU(IChunkLayout layout, ComputeBuffer Triangle, ComputeBuffer Args, List<ChunkKey> keys)
     {
+        this.Layout = layout;
         this.Triangle = Triangle;
         this.Args = Args;
-        this.Bounds = this.ComputeBounds(contexts);
+        this.Bounds = this.ComputeBounds(keys);
     }
 
     public void Dispose()
@@ -28,17 +30,17 @@ public class ChunkRegionRenderDataGPU
         Graphics.DrawProceduralIndirectNow(MeshTopology.Triangles, Args, 0);
     }
 
-    private Bounds ComputeBounds(List<ChunkContext> chunkContexts)
+    private Bounds ComputeBounds(List<ChunkKey> chunkContexts)
     {
         if (chunkContexts.Count == 0)
             return new Bounds(Vector3.zero, Vector3.zero);
 
-        Vector3 min = chunkContexts[0].WorldPosition;
-        Vector3 max = chunkContexts[0].WorldPosition;
+        Vector3 min = this.Layout.ToWorld(chunkContexts[0]);
+        Vector3 max = min;
 
         foreach (var ctx in chunkContexts)
         {
-            Vector3 pos = ctx.WorldPosition;
+            Vector3 pos = this.Layout.ToWorld(ctx);
             min = Vector3.Min(min, pos);
             max = Vector3.Max(max, pos);
         }
